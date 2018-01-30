2683482ee7e106b79a8be1077b185cee83d82396
Gabriela Leal <gabriela.leal@hotmail.com>
2018-01-30 17:25:48 -0300

    mais ajustes

--- MandacaruAerodesign/static/js/script.js
+++ MandacaruAerodesign/static/js/script.js
@@ -2,7 +2,7 @@ $(function(){
 	$(document).ready(function($){
 		$('.scroll').on('click', function(event){
 			event.preventDefault();
-			$('html,body').animate({scrollTop:$(this.hash).offset().top -105}, 500);
+			$('html,body').animate({scrollTop:$(this.hash).offset().top -105}, 300);
 		});
 	});
 	$('#menumobile').on('click', function (){
